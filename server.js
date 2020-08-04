f3452f9918e565b1d6988830ae50e2bd39d90d7e
mako4kamiya <mako4kamiya@gmail.com>
2020-08-05 00:25:53 +0900

    mongooseを使うための設定とCreateの作成

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.use(bodyParser.json()); //Express4.16以降 app.use(express.json()) https://
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true })); //Express4.16以降 app.use(express.urlencoded({ extended: true }))
 
+const db = require("./app/models");
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
+
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to bezkoder application." });
